4d8ecbd27efecd6cfe049507b1b12c0852df4c8b
Greg Nittner <Grzegorz.Nittner@externals.hilti.com>
2025-12-08 14:39:24 +0100

    initial commit with spring generated files

---
+++ src/main/java/eu/nittner/webinspector/WebInspectorApplication.java
@@ -0,0 +1,13 @@
+package eu.nittner.webinspector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebInspectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebInspectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/nittner/webinspector/WebInspectorApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.nittner.webinspector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebInspectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
